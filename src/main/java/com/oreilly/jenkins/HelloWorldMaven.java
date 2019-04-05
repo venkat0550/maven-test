a05d396629fc5e295845bf1fa9486f6cabff2918
venkat0550 <venkat.lab@outlook.com>
2019-04-05 16:10:31 +0530

    modified java and pom to depend on maven-test-dependcnies jar file

--- src/main/java/com/oreilly/jenkins/HelloWorldMaven.java
+++ src/main/java/com/oreilly/jenkins/HelloWorldMaven.java
@@ -3,6 +3,8 @@ package com.oreilly.jenkins;
 public class HelloWorldMaven {
 
 	public static void main(String[] args) {
+		MessageSource msgSource = new MessageSource();
+		System.out.println(msgSource.getMessage());
 		System.out.println("Hello from a Maven project.");
 	}
 }
